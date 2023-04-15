3258fd3aa81f518b9396f5c816e15f614e910f2e
Kateryna <k.borysenko.kyiv@gmail.com>
2023-04-15 14:21:05 +0300

    added animaied gradient border components

---
+++ README.md
@@ -0,0 +1,3 @@
+# Animated Gradient Border
+
+ <img src="./src/screenshot.png" width="900" alt="screenshot">
---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import s from './App.module.css';
+
+function App() {
+  return (
+    <>
+      <div class={s.gradientBorder} >Animated Gradient Border - 1 </div>
+      <div class={s.demo}></div>
+    </>
+  );
+}
+
+export default App;
